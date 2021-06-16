8bc6d491ecdb4b0098978083003bc26243c9317a
gudakes-h <85408756+gudakes-h@users.noreply.github.com>
2021-06-16 20:36:15 +0530

    Update index.js

--- Day6/index.js
+++ Day6/index.js
@@ -1,5 +1,4 @@
 
-
 function is_array(p1) {
     
      return Array.isArray(p1);   
@@ -8,7 +7,7 @@ function is_array(p1) {
 function array_Clone(p1) {
     
      p1Copy = p1.splice(0, p1.length);
-     return p1Copy;   
+     console.log(p1Copy);   
   }
 
 function first(p1, p2) {
@@ -24,13 +23,14 @@ function first(p1, p2) {
   }
 
   
-function first(p1) {
+function first1(p1) {
     
     return p1[0];   
  }
 
 function concat(p1) {
      
+     console.log(p1.toString());
      return p1.toString();   
   }
 
@@ -52,11 +52,24 @@ function most(arr1){
             }
             m=0;   
     }
+    console.log(item);
 }
  
 
 console.log(is_array('w3resource'));
 console.log(is_array([1, 2, 4, 0]));
-console.log(array_Clone([1, 2, 4, 0]));
-console.log(array_Clone([1, 2, [4, 0]]));
+array_Clone([1, 2, 4, 0]);
+array_Clone([1, 2, [4, 0]]);
+console.log(first1([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+
+
+myColor = ["Red", "Green", "White", "Black"];
+concat(myColor)
+
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+most(arr1)
 
