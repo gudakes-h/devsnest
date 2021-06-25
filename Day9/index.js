716c7b6767e61b64547625cd82801ceed04411f5
gudakes-h <85408756+gudakes-h@users.noreply.github.com>
2021-06-25 22:58:53 +0530

    Update index.js

--- Day9/index.js
+++ Day9/index.js
@@ -4,13 +4,13 @@ for(let i=1; i<=36; i++){
 }
 let remaining=36;
 let gone=0;
-let btns=document.querySelectorAll(".grid > div");
+let buttons=document.querySelectorAll(".grid > div");
 let stats=document.querySelectorAll("span");
 console.log(stats);
 
 for(let i=0; i<36; i++){
-    btns[i].addEventListener("click", (e)=>{
-        if(btns[i].classList.length){
+    buttons[i].addEventListener("click", (e)=>{
+        if(buttons[i].classList.length){
             remaining++;
             gone--;
             stats[0].innerHTML=gone;
@@ -21,5 +21,6 @@ for(let i=0; i<36; i++){
             stats[0].innerHTML=gone;
             stats[1].innerHTML=remaining;
         }
-        btns[i].classList.toggle("active");
+        buttons[i].classList.toggle("active");
     })
+}
